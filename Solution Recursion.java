c38818894dc93d4036f00d7161f3786a13376af6
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-04-17 08:07:34 +0200

    Create Solution Recursion.java

---
+++ Solution Recursion.java
@@ -0,0 +1,37 @@
+
+public class Solution {
+
+    private int sum;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
+
+// Class TreeNode is in-built in the solution file on leetcode.com. 
+// When running the code on the website, do not include this class.
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
